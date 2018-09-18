b59cbc15f7f27b52ccc02523ed0e0621f1dceaed
VFerrari <vferrari@mpc.com.br>
2018-09-17 23:25:15 -0300

    Primeira tentativa com listas e tipos definidos

--- Haskell/Projeto/waze.hs
+++ Haskell/Projeto/waze.hs
@@ -23,12 +23,35 @@
 -- Junta todas as arestas entre 2 nós em uma, com peso igual ao menor.
 -- Menor peso: a pé ou soma da metade do período do t. publico e o peso normal.
 
-import Data.Map.Strict as Map
-import Data.Maybe
+import Data.List (sortBy)
+
+-- Lista de adjacências. Grafo é uma lista de "No"
+data Aresta = Aresta Char String Float deriving (Show, Eq, Ord)
+data No = No Char [Aresta] deriving (Show, Eq)
 
 main = do 
        input <- getContents
+       let inputSplit = map words $ lines input
        putStrLn $ head $ lines input -- Placeholder
 
--- Insere uma aresta no grafo
-readInput g (or:de:modo:peso) = Map.insert de (modo,peso) $ fromMaybe $ Map.lookup or
+-- Ler o grafo a partir do input dividido em palavras
+readInput :: [No] -> [String] -> [No]
+readInput g (or:stDe:mo:stPe) = 
+    let de = read stDe :: Char 
+        pe = read stPe :: Float
+    in insAresta or (Aresta de mo pe) g 
+   
+
+-- Inserir aresta no grafo
+insAresta origem aresta [] = [No origem [aresta]]
+insAresta origem aresta ((No b l):gs)
+    | b == origem = ((No b (aresta:l)):gs)
+    | b /= origem = ((No b l):insAresta origem aresta gs)
+
+-- Função auxiliar para peso de aresta.
+getFloat (Aresta _ _ f) = f
+
+-- Função de comparação de arestas para uso com SortBy
+sortFloat (Aresta _ _ f1) (Aresta _ _ f2)
+    | f1 > f2 = GT
+    | otherwise = LT
