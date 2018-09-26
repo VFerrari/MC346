bc5c89bcb6c340c0ffa8e8b7ab9d8093b340574a
VFerrari <vferrari@mpc.com.br>
2018-09-26 18:11:30 -0300

    Modelagem do grafo refeita: grafo simples criado

--- Haskell/Projeto/waze.hs
+++ Haskell/Projeto/waze.hs
@@ -8,19 +8,10 @@
 -- Modelagem por meio de grafo, e o menor caminho entre dois pontos (um para um).
 -- Retorna o melhor caminho, o modo de chegar a cada ponto, e o tempo total gasto.
 
--- TODO: Modelar o grafo para simples implementação do algoritmo de Djikstra
-    -- tecnicamente feito
 -- TODO: Djikstra
 -- TODO: Resposta
 
--- * = Mais urgentes
-
--- Ideias para modelagem:
--- Junta todas as arestas entre 2 nós em uma, com peso igual ao menor.
--- Menor peso: a pé ou soma da metade do período do t. publico e o peso normal.
-
 import Data.Map.Strict as Map
-import Data.List(sortBy)
 import Data.Maybe
 
 main = do
@@ -31,40 +22,45 @@ main = do
        
        -- Separando a entrada em tres seções
        let gInput = takeWhile (not . Prelude.null) linInput -- Grafo
+       let prettyInput = Prelude.map (\(ori:dest:modo:peso:[]) -> (ori,dest,modo,peso)) $ Prelude.map words gInput
+       
        let modInput = (init . init . tail) $ dropWhile (not . Prelude.null) linInput
        let pathInput = words $ last linInput -- Caminho desejado
-       
-       -- Temporário: imprime a entrada dividida
-    --    print gInput
-    --    print modInput
-    --    print pathInput
-    --    putStrLn ""
        
        -- Cria o dicionário de modificadores
        let mapModo = fromList $ Prelude.map (mapLines) $ Prelude.map (words) modInput
 
        -- Lê o grafo e o ordena
-       let modGraph = shortGraph $ readGraph gInput mapModo
+       let graph = readGraph prettyInput mapModo empty
 
-       print modGraph
-
+       print graph
+       
 -- Transforma uma linha da entrada com os modificadores em uma tupla.
--- A tupla contém uma string e um float (convertido de string).
 mapLines (a:b:[]) = (a,c) where c = read b :: Float
-
--- Insere uma aresta no grafo a partir de uma linha da entrada
--- Insere o vértice de origem se não existe no grafo.
--- Aplica os modificadores às arestas correspondentes (período do t. publico/2).
--- Recebe o dicionário de modificadores, o grafo e uma linha da entrada, retornando o grafo.
-readInput g mapModo (ori:dest:modo:strPeso:[])  = insert ori (insertEdge $ Map.lookup ori g) g
-    where peso = read strPeso :: Float
-          modif = Map.lookup modo mapModo                                           -- Encontra modificador (se existe) no dicionário
-          pesoReal = peso + (fromMaybe 0.0 modif)/2                                             
-          insertEdge m = insertWith (++) dest [(modo,pesoReal)] $ fromMaybe empty m -- Insere a aresta, concatenando a tupla (modo,peso) na lista.
 
 -- Função que percorre a entrada, construindo o grafo.
 -- O grafo é modelado como uma lista de adjacências, feito com Maps no lugar de listas.
-readGraph inp modos = Prelude.foldl(\g lin -> readInput g modos $ words lin) empty inp
+readGraph inp modos og = Prelude.foldl(\g lin -> readEdge g modos lin) og inp
 
--- Função que ordena as arestas por peso.
-shortGraph oriGraph =  Map.map (Map.map (sortBy (\(_,a) (_,b) -> compare a b))) oriGraph
+-- Função que lê a aresta a ser inserida
+-- Verifica o modo, e realiza as ações pertinentes a cada uma.
+-- Insere a aresta "a-pe", ou faz uma sequencias de inserções de transporte publico.
+-- Cria aresta de "mudança de plano", que simboliza entrar em um ônibus.
+-- O preço dessa mudança está no dicionário mapModos.
+readEdge g mapModos (ori,dest,modo,strPeso)
+    | modo == "a-pe" = insEdge (ori,dest,modo,peso) "a-pe" g
+    | otherwise = insEdge (dest,dest,modo,0.0) "a-pe" $ 
+                  insEdge (ori,dest,modo,peso) modo $ 
+                  insEdge (ori,ori,"a-pe",modif) modo g
+        
+    where peso = read strPeso :: Float
+          modif = (fromJust $ Map.lookup modo mapModos)/2
+
+-- Insere uma aresta no grafo a partir de uma linha da entrada
+-- Insere o vértice de origem se não existe no grafo.
+-- Recebe a tupla de 4 elementos que representa a aresta.
+-- Recebe o "plano" do vértice destino (modo de entrada no vértice).
+insEdge (ori,dest,modoSaida,peso) modoEnt g 
+    = insert (ori,modoSaida) (insertEdge $ Map.lookup (ori,modoSaida) g) g
+    where insertEdge m = insert (dest,modoEnt) peso $ fromMaybe empty m                 
+    -- Insere a aresta, gerando o par (destino,modo) e o seu peso como valor.
