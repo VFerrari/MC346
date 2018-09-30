780f4e760976da4df780cb2d2529416a7480ee61
VFerrari <vferrari@mpc.com.br>
2018-09-30 17:37:01 -0300

    Projeto completo, atualização no README

--- Haskell/Projeto/README.md
+++ Haskell/Projeto/README.md
@@ -1,31 +1,48 @@
-# Project 1 - "Waze for public transport"
+# Projeto 1 - "Waze Para Transporte Público"
 
  <img align = "left" src= https://www.baeldung.com/wp-content/uploads/2017/01/initial-graph.png width="456" height="190"  hspace="40" vspace="5" alt="Borin Png">
 
-> ***“Precisamos subir o nível de elegância”** <br /> ― Wainer, Jacques*
+> ***“Precisamos subir o nível de elegância!”** <br /> ― Wainer, Jacques*
 
-> ***“Essa frase ta aqui para nao quebrar a imagem”** <br /> ― Ferragut, Daniel*
+> ***“Haskell: a única linguagem onde é mais fácil implementar um dicionário como fila de prioridades do que um heap.”** <br /> ― Ferragut, Daniel*
 
-> ***“Another one”** <br /> ― Khaled, Dj*
+> ***“Posso comentar? Mudar os nomes? Refazer o código?”** <br /> ― Ferrari, Victor*
+
+## Autores
+[Daniel Ferragut](https://github.com/danielferragut) e [Victor Ferrari](https://github.com/VFerrari)
 
 ## Objetivos
 Programa em Haskell que calcula o caminho mais rápido entre uma origem e um destino dado usando uma combinação de caminhar e usar transporte público.
 
 Modelagem por meio de grafo, e o menor caminho entre dois pontos (um para um).
 Retorna o melhor caminho, o modo de chegar a cada ponto, e o tempo total gasto.
+## Método
+Para calcular o caminho mínimo, foi utilizado o Algoritmo de Djikstra.
+
+Como o algoritmo é aplicado em grafos simples, o grafo foi modelado de modo a ser simples.
+Para isso, cada vértice foi representado como uma tupla de duas strings (nome e modo).
+Cada modo de transporte foi assim transformado em um "plano" diferente, com apenas os vértices alcançáveis pela linha, assim removendo a necessidade de arestas múltiplas entre vértices.
+O custo de entrar em um desses planos é metade do período do modo (dado na entrada), e o custo de saída é zero.
+
+O grafo foi modelado como uma lista de adjacências, mas com estrutura de dicionários.
+Isso foi interessante pois possibilitou o uso de muitas funções prontas na biblioteca Data.Map.Strict.
+
+Mais informações podem ser vistas nos comentários do código (arquivo `waze.hs`).
 ## Enunciado
 O enunciado pode ser encontrado no arquivo `enunciado.html`.
 
 Para visualizar o arquivo como uma página sem baixá-lo, utilize a ferramenta [HTMLPreview](http://htmlpreview.github.io/).
 
+Autor: [Jacques Wainer](https://ic.unicamp.br/~wainer).
+
 ## Referências utilizadas
 * [Learn You Haskell](http://learnyouahaskell.com/chapters) - Livro texto usado para Haskell
+* [Algoritmo de Djikstra](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm) - Famoso algoritmo para cálculo de caminhos mínimos em grafos simples.
+* [Data.Map.Strict](http://hackage.haskell.org/package/containers-0.6.0.1/docs/Data-Map-Strict.html) - Biblioteca de dicionários padrão da linguagem.
 
-
-## Coisas que restam fazer
-  -  [ ] Criar o tipo do grafo.
-  -  [ ] Ler o grafo e as características.
-  -  [ ] Biblioteca simples para operações em grafo (existe o Data.Graph mas não parece ser o que queremos).
-  -  [ ] Modelar o grafo para simples implementação do algoritmo de Djikstra
-  -  [ ] Djikstra
-  -  [ ] Resposta
+## Coisas que restam fazer.
+  -  [x] Definir o tipo do grafo;
+  -  [x] Ler o grafo e as características;
+  -  [x] Modelar o grafo para simples implementação do algoritmo de Djikstra;
+  -  [x] Implementar o algoritmo de Djikstra;
+  -  [x] Encontrar o caminho e imprimir resposta.
--- Haskell/Projeto/waze.hs
+++ Haskell/Projeto/waze.hs
@@ -6,44 +6,40 @@
 -- e um destino dado usando uma combinação de caminhar e usar transporte público.
 
 -- Modelagem por meio de grafo, e o menor caminho entre dois pontos (um para um).
+-- Grafo simplificado transformando cada modo de transporte em um "plano" distinto, com os vértices da linha.
 -- Retorna o melhor caminho, o modo de chegar a cada ponto, e o tempo total gasto.
 
--- TODO: Resposta
--- TODO: Fazer djikstra parar quando encontrar o vertice final (talvez).
+-- Modificado em: 30/09/2018
 
 import Data.Map.Strict as Map
 import Data.List(minimumBy)
 import Data.Maybe
 
 main = do
-
-       -- Lendo a entrada
        input <- getContents 
        let linInput = lines input 
        
-       -- Separando a entrada em tres seções
+       -- Separando a entrada em quatro seções:
        let gInput = Prelude.map words $ takeWhile (not . Prelude.null) linInput       -- Grafo
        let waitInput = (init . init . tail) $ dropWhile (not . Prelude.null) linInput -- Periodos
-       let pathInput = words $ last linInput                                          -- Entrada e Final
+       let pathInput = words $ last linInput                                          -- Começo e Final
+       let start = (head pathInput,"a-pe")                                            -- Começo
+       let end = (last pathInput,"a-pe")                                              -- Final
        
        -- Cria o dicionário de tempos de espera
-       let mapWait = fromList $ Prelude.map (mapLines) $ Prelude.map words waitInput
-
-       -- Lê o grafo como lista de adjacências, e aplica o algoritmo de Djikstra no grafo.
-       let graph = readGraph gInput mapWait empty
-       let prev = djikstra (head pathInput, "a-pe") graph
-       let caminho = pathList prev (last pathInput) "a-pe" []
-       let prettyOutput = parsePath caminho (head pathInput)
-       let peso = calcularPeso graph caminho 0
-
-       putStrLn prettyOutput
-       print peso
+       -- Lê o grafo como lista de adjacências, e aplica o algoritmo de Djikstra.
+       let (ant,tam) = djikstra start end $ readGraph gInput empty $ fromList $
+                       Prelude.map (mapLines) $ Prelude.map words waitInput
+      
+       -- Percorre a árvore de antecessores, e retorna o caminho.
+       putStrLn $ parsePath (fst start) $ pathList end [] ant
+       print tam
        
 -- Transforma uma linha da entrada com os periodos em uma tupla (com valor periodo/2).
 mapLines (a:b:[]) = (a,c) where c = (read b :: Float)/2
 
 -- O grafo é modelado como uma lista de adjacências, feito com Maps no lugar de listas.
-readGraph inp modes og = Prelude.foldl(\g lin -> readEdge g modes lin) og inp
+readGraph inp og modes = Prelude.foldl(\g lin -> readEdge g modes lin) og inp
 
 -- Função que lê a aresta a ser inserida
 -- Insere, se necessario, os vértices envolvidos nessa aresta.
@@ -54,12 +50,11 @@ readGraph inp modes og = Prelude.foldl(\g lin -> readEdge g modes lin) og inp
 readEdge g mapWait (ori:end:mode:strWei:[])
     | mode == "a-pe" = insEdge (ori,end,"a-pe",wei) "a-pe" $
                        insertIfNotMember (end, "a-pe") g
-    
+                       
     | otherwise = insEdge (end,end,mode,0.0) "a-pe" $ 
                   insertIfNotMember (end, "a-pe") $
                   insEdge (ori,end,mode,wei) mode $ 
                   insEdge (ori,ori,"a-pe",wait) mode g
-        
     where wei = read strWei :: Float
           wait = fromJust $ Map.lookup mode mapWait
 
@@ -78,25 +73,26 @@ insEdge (ori,end,exitMode,wei) entryMode g
 -- Encontra o menor caminho entre um vértice e todos os outros.
 -- Recebe o vértice inicial e o grafo, e retorna a árvore de antecessores.
 -- Inicia o dicionário de predecessores com "nil" e o de distâncias com infinito.
-djikstra start graph = djikstraLoop graph (prev,dist)
-    where prev = initialize graph ("nil","nil")
+djikstra start end graph = djikstraLoop graph end (pred,dist)
+    where pred = initialize graph ("nil","nil")
           dist = insert start 0 $ initialize graph (1/0) -- 1/0 == infinity
 
 -- Loop principal do Djikstra.
 -- Se a fila de prioridades estiver vazia, retorna a árvore de predecessores.
+-- Se encontrar o vértice final, retorna a árvore e a distância total.
 -- Senão, extrai o menor elemento, encontra seus vizinhos no grafo, e relaxa as arestas.
-djikstraLoop graph (prev,dist)
-    | dist == empty = prev 
-    | otherwise =
-    
-    -- Extrai o mínimo, e encontra o vizinho desse vértice mínimo.
-    let (u , distU) = minimumBy (\x y-> compare (snd x) (snd y)) $ toList dist
-        delDist = delete u dist
-        neigh = fromJust $ Map.lookup u graph 
+djikstraLoop graph end (pred,dist)
+    | dist == empty = (pred,(1/0))
+    | u == end = (pred,distU)
     
     -- Relaxa as arestas, e passa ao próximo vértice, recursivamente.
-    in  djikstraLoop graph $
-        Map.foldlWithKey (\(currPrev,currDist) v wei -> relax (u,distU) (v,wei) (currPrev,currDist)) (prev,delDist) neigh
+    | otherwise = djikstraLoop graph end $
+                  Map.foldlWithKey (\(currPred,currDist) v wei -> relax (u,distU) (v,wei) (currPred,currDist)) (pred,delDist) neigh
+
+    -- Extrai o mínimo, e encontra o vizinho desse vértice mínimo.
+    where (u , distU) = minimumBy (\x y-> compare (snd x) (snd y)) $ toList dist
+          delDist = delete u dist
+          neigh = fromJust $ Map.lookup u graph 
 
 -- Inicia um dicionário com um valor padrão passado.
 initialize graph newValue = Map.map (\ _ -> newValue) graph
@@ -104,35 +100,32 @@ initialize graph newValue = Map.map (\ _ -> newValue) graph
 -- Função de relaxação de aresta.
 -- Recebe o vértice de origem/distância calculada até ele (u, distU)
 -- Recebe o vértice de destino/peso da aresta a ele (v,wei)
--- Recebe os dicionários de antecessores e distâncias (prev,dist).
+-- Recebe os dicionários de antecessores e distâncias (pred,dist).
 -- Atualiza o antecessor e a distância de V se d(V) > d(U) + wei
-relax (u,distU) (v,wei) (prev,dist)
-    | dV > d = (newPrev , newDist)
-    | otherwise = (prev, dist)
+relax (u,distU) (v,wei) (pred,dist)
+    | dV > d = (newPred , newDist)
+    | otherwise = (pred, dist)
     where dV = fromMaybe (-1/0) $ Map.lookup v dist
           d = distU + wei
-          newPrev = insert v u prev
+          newPred = insert v u pred
           newDist = insert v d dist
 
--- pathList :: Map  ([Char], [Char]) ([Char], [Char]) -> ([Char], [Char]) -> [([Char], [Char])] -> [([Char], [Char])]
-pathList prev "nil" "nil" lista = lista
-pathList prev vertice modo lista =
-    let ponto = (vertice, modo)
-        listaMod = ponto:lista
-        (vertPrev,modoPrev)= fromJust $ Map.lookup ponto prev
-    in  pathList prev vertPrev modoPrev listaMod
+-- Função que retorna o menor caminho, em forma de lista.
+-- Percorre árvore de predecessores, adicionando-os à lista.
+pathList ("nil","nil") list _ = list
+pathList vert list pred =
+    let newList = vert:list
+        prevVert = fromJust $ Map.lookup vert pred
+    in  pathList prevVert newList pred
 
-parsePath ([a]) texto = texto
-parsePath (a:b:xs) texto
-    | modoOri == modoDest = if modoOri == "a-pe" then parsePath (b:xs) ape else parsePath (b:xs) texto
-    | otherwise = if modoDest == "a-pe" then parsePath (b:xs) descer else  parsePath (b:xs) texto
-    where   (vertOri, modoOri) = a 
-            (vertDest, modoDest) = b
-            ape = texto ++ (" a-pe " ++ vertDest)
-            descer = texto ++ ((" " ++ modoOri) ++ (" " ++ vertDest))
-
-calcularPeso gInput ([a]) acc = acc
-calcularPeso gInput (a:b:xs) acc = 
-    let peso = fromJust $ Map.lookup b $ fromJust $ Map.lookup a gInput
-    in  calcularPeso gInput (b:xs) (acc+peso)
-
+-- Função que transforma a lista de menor caminho em uma string.
+-- Cria a saída do programa, separando cada vértice e modo.
+-- Exclui as "trocas de modo", onde os vértices são iguais.
+parsePath str ([_]) = str
+parsePath str (h:t:xs)
+    | headM == tailM = if headM == "a-pe" then parsePath walk (t:xs) else parsePath str (t:xs)
+    | otherwise = if tailM == "a-pe" then parsePath out (t:xs) else  parsePath str (t:xs)
+    where   (_, headM) = h 
+            (tailV, tailM) = t
+            walk = str ++ " a-pe " ++ tailV
+            out = str ++ (' ':headM) ++ (' ':tailV)
