74f47d4244980542b8a5b5bcbdfb08dffb8b8b2d
VFerrari <vferrari@mpc.com.br>
2018-10-04 15:45:38 -0300

    Exercicios extras aula 14.

--- Prolog/Exercicios/aula14.pl
+++ Prolog/Exercicios/aula14.pl
@@ -20,5 +20,23 @@ pai(c,f).
 ant(A,B) :- pai(A,B).
 ant(A,B) :- pai(A,C),ant(C,B).
 
-% Exercicios:
-% Não tem...
+% Exercicios extras:
+% Soma dos valores de uma lista numérica (alto nivel)
+soma(X,Y,R) :- R is X+Y.
+somaL(X,R) :- foldl(soma,X,0,R).
+
+% Conta elementos de uma lista (alto nível)
+plus1(_,X,R) :- R is X+1.
+tam(X,R) :- foldl(plus1,X,0,R).
+
+% Soma numeros pares de uma lista (alto nível)
+somaPar(X,A,R) :- L is mod(X,2), (L == 0 -> R is A+X ; R is A).
+somap(X,R) :- foldl(somaPar,X,0,R).
+
+% Conta quantas vezes um item aparece na lista (alto nível)
+igual(X,X).
+amount(X,L,R) :- include(igual(X),L,RR), tam(RR,R).
+
+% Remove item da lista (todas as vezes, alto nível)
+diferente(X,Y) :- X \= Y.
+rmAll(I,L,R) :- include(diferente(I),L,R).
