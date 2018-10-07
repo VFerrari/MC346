b704aee777c6b3efe6a2b60e48fd16fbb1342476
VFerrari <vferrari@mpc.com.br>
2018-10-06 21:28:48 -0300

    Mudando a memorização para dicionário como visto em classe

--- Prolog/Projeto/inter.pl
+++ Prolog/Projeto/inter.pl
@@ -10,18 +10,26 @@
 :- dynamic inter_dic/3.
 
 % Temporaria: transforma em lista de chars e chama intersect.
-interchar(X,Y,R) :- retractall(known_inter(_,_,_)), string_chars(X,XX), string_chars(Y,YY), intersect(XX,YY,RR), string_chars(R,RR).
+interchar(X,Y,R) :- clearall(), string_chars(X,XX), string_chars(Y,YY), intersect(XX,YY,RR), string_chars(R,RR).
 
 % Retorna a lista sem o ultimo elemento
 init([_],[]).
 init([X|XS],[X|R]) :- init(XS,R), !.
 
-% Encontra interseção entre duas strings
+% Encontra maior interseção entre duas listas.
 intersect([],_,[]).
 intersect(_,[],[]).
 intersect(X,X,X).
-intersect(X,Y,R) :- inter_dic(X,Y,R), !.
-intersect([X|XS],Y,R) :- intersect(XS,Y,RR), init(Y,YY), intersect([X|XS],YY,RRR), long(RR,RRR,R), !, asserta(inter_dic([X|XS],Y,R)).
+intersect(X,Y,R) :- get(X,Y,R), !.
+intersect([X|XS],Y,R) :-  init(Y,YY), 
+                          intersect(XS,Y,RR), intersect([X|XS],YY,RRR), 
+                          long(RR,RRR,R), !, set([X|XS],Y,R).
 
 % Dadas duas listas, determina a mais longa.
 long(X,Y,R) :- length(X,XX), length(Y,YY), (XX > YY -> R = X ; R = Y).
+
+
+% Funções para manipulação de dicionário
+get(CH1,CH2,V) :- inter_dic(CH1,CH2,V).
+set(CH1,CH2,V) :- asserta(inter_dic(CH1,CH2,V)).
+clearall() :- retractall(inter_dic(_,_,_)).
